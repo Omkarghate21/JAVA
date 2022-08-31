079aece483c136cd2d50fee221df4b0d10e16797
Omkarghate21 <112567713+Omkarghate21@users.noreply.github.com>
2022-08-31 22:11:33 +0530

    Add files via upload

---
+++ p3.java
@@ -0,0 +1,7 @@
+class p3{
+	public static void main(String args[]){
+		int a=1,b=2;
+		int res=((a+b)*(a+b))-(2*a*b);
+		System.out.print("a^2+b^2 = "+res);
+	}
+}
---
+++ p4.java
@@ -0,0 +1,7 @@
+class p4{
+	public static void main(String args[]){
+		int a=5,b=6;
+		int res= (a+b)-(a-b);
+		System.out.println(" a^2-b^2 ="+res);
+	}
+}
